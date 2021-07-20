75d9f088622fb15a169bf04c64ef0bed09ed1b3a
ashu7227 <ashu5915@gmail.com>
2021-07-20 14:21:45 +0530

    adding

---
+++ src/test/java/com/cicd/docker/cicdDocker/CicdDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.docker.cicdDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
