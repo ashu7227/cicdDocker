859073d06e951ecfe7a7d913a9881767284fda51
ashu7227 <ashu5915@gmail.com>
2021-07-20 14:24:58 +0530

    commit

--- src/main/java/com/cicd/docker/CicdDockerApplication.java
+++ src/main/java/com/cicd/docker/CicdDockerApplication.java
@@ -1,4 +1,4 @@
-package com.cicd.docker.cicdDocker;
+package com.cicd.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
